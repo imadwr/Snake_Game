355070204eab74484f9702c03ecc8477522f21df
imadwr <imadouairem96@gmail.com>
2021-04-26 14:36:05 +0000

    extend snake, detect collision with wall, detect collision with tail

--- scoreboard.py
+++ scoreboard.py
@@ -11,6 +11,10 @@ class Scoreboard(Turtle):
         self.score = 0
         self.write_score()
 
+    def game_over(self):
+        self.goto(x=0, y=0)
+        self.write("Game Over.", align=SCORE_ALIGNMENT, font=SCORE_FONT)
+
     def write_score(self):
         self.clear()
         self.goto(x=0, y=270)
