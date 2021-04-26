19495ffbb561b09c61baa332e91b5643822a461b
imadwr <imadouairem96@gmail.com>
2021-04-26 13:31:32 +0000

    Create a scoreboard and track score.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from turtle import Screen
 from snake import Snake
 from food import Food
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
@@ -11,6 +12,7 @@ screen.title("Snake Game")
 
 snake = Snake()
 food = Food()
+scoreboard = Scoreboard()
 
 screen.listen()
 
@@ -28,6 +30,8 @@ while game_is_on:
 
     if snake.head.distance(food) < 15:
         food.refresh()
+        scoreboard.score += 1
+        scoreboard.write_score()
 
 
 
---
+++ scoreboard.py
@@ -0,0 +1,17 @@
+from turtle import Turtle
+SCORE_ALIGNMENT = "center"
+SCORE_FONT = ("Courier", 18, "bold")
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.color("white")
+        self.penup()
+        self.hideturtle()
+        self.speed("fastest")
+        self.score = 0
+        self.write_score()
+
+    def write_score(self):
+        self.clear()
+        self.goto(x=0, y=270)
+        self.write(f"Score: {self.score}", align=SCORE_ALIGNMENT, font=SCORE_FONT)
